2083aa26410e54eef6305e5f5a0af1e80d605fb1
FlyCloudC <52472693+FlyCloudC@users.noreply.github.com>
2020-02-06 14:34:58 +0800

    Update script.js
    rename "map" to "urlList"

--- utils/baidu_tts/script.js
+++ utils/baidu_tts/script.js
@@ -1,16 +1,16 @@
 let audio, showP;
-let map = new Map();
+let urlList = new Map();
 
 window.onload = function () {
   audio = document.getElementById('preview-audio');
   showP = document.getElementById('show-url');
   for (let key of ['spd', 'pit', 'vol', 'per', 'tex'])
-    map.set(key, document.getElementById(key));
+    urlList.set(key, document.getElementById(key));
 }
 
 function say() {
   let url = 'http://tts.baidu.com/text2audio?cuid=baike&lan=zh&ctp=1&pdt=301';
-  for (let [key, { value }] of map)
+  for (let [key, { value }] of urlList)
     if(value)
       url += `&${key}=${value}`;
   audio.src = url;
