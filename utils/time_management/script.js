a33870c53271ae8f1f9f98c168b57bf39f675b2b
flycloudc <1812402763@qq.com>
2020-08-15 12:00:32 +0800

    统一格式。工具添加时间管理

---
+++ utils/time_management/script.js
@@ -0,0 +1,9 @@
+﻿const UI = {};
+//const Data;
+
+window.onload = () => {
+  [UI.taskDiv, UI.countdownDiv, UI.statisticDiv] =
+    ['task-list', 'countdown-list', 'statistic-list']
+      .map(x => document.getElementById(x));
+
+}
